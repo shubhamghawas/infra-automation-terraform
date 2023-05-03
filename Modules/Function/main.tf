1e87a584fc706b4fffd3c0942647219edb121921
Shubham Ghawas <shubham.ghawas@publicisgroupedev.net>
2023-05-03 14:53:52 +0530

    .

--- Modules/Function/main.tf
+++ Modules/Function/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = var.storage_account_replication_type
 }
 resource "azurerm_app_service_plan" "asp" {
-  name                = var.function_app_name + "-asp"
+  name                = "${var.function_app_name}-asp"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku {
